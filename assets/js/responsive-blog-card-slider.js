43dda4de81110bcb0d8349ac224f9436e3f6f432
Hana Ibrahim <hanaibrahimmustafa@gmail.com>
2023-02-08 17:14:34 +0300

    update News page and index page

--- assets/js/responsive-blog-card-slider.js
+++ assets/js/responsive-blog-card-slider.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".blog-slider", {
+var swiper2 = new Swiper(".blog-slider", {
   spaceBetween: 30,
   effect: "fade",
   loop: true,
@@ -10,4 +10,34 @@ var swiper = new Swiper(".blog-slider", {
     el: ".blog-slider__pagination",
     clickable: true
   }
+});
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
 });
